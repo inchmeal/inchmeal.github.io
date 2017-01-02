7551f2131f4b7d1b4c0dade8ecf56f003d993644
0vais <mailovais85@gmail.com>
2017-01-02 18:14:00 +0530

    Moved personal blogs to different url.

--- app/_plugins/WordsGenerator.rb
+++ app/_plugins/WordsGenerator.rb
@@ -4,14 +4,16 @@ class MySubnavGenerator < Jekyll::Generator
   def generate(site)
     parser = Jekyll::Converters::Markdown.new(site.config)
     site.data["totalWords"] = 0
-    site.collections["vocab"].each do |page|
-        if page.ext == ".markdown"
-        doc = Nokogiri::HTML(parser.convert(page.content))
-        page.data["words"] = []
-        doc.css('[word]').each do |heading|
-          page.data["words"] << { "title" => heading.text, "url" => [page.url, heading['id']].join("#") }
-        end
-        site.data["totalWords"] = site.data["totalWords"] + page.data["words"].size
+    unless site.collections["vocab"].nil?
+      site.collections["vocab"].each do |page|
+          if page.ext == ".markdown"
+            doc = Nokogiri::HTML(parser.convert(page.content))
+            page.data["words"] = []
+            doc.css('[word]').each do |heading|
+              page.data["words"] << { "title" => heading.text, "url" => [page.url, heading['id']].join("#") }
+            end
+            site.data["totalWords"] = site.data["totalWords"] + page.data["words"].size
+          end
       end
     end
   end
